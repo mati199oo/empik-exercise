568d1c0de6547a9a98aff408699981cd0c6e9773
mati199oo <mati199oo@gmail.com>
2021-05-17 19:00:33 +0200

    Final touches and changing unnecessary code into more fancy one.

--- src/main/java/component/database/model/LoginCounter.java
+++ src/main/java/component/database/model/LoginCounter.java
@@ -1,16 +1,19 @@
 package component.database.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.util.Objects;
 
 @Getter
 @Setter
+@EqualsAndHashCode
+@ToString
 @Entity
 @Table(name = "EXERCISE")
 public class LoginCounter {
@@ -23,30 +26,4 @@ public class LoginCounter {
     private Integer requestCount;
 
     public LoginCounter() {}
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof LoginCounter)) return false;
-
-        LoginCounter that = (LoginCounter) o;
-
-        if (!Objects.equals(login, that.login)) return false;
-        return Objects.equals(requestCount, that.requestCount);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = login != null ? login.hashCode() : 0;
-        result = 31 * result + (requestCount != null ? requestCount.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "LoginCounter{" +
-                "login='" + login + '\'' +
-                ", requestCount=" + requestCount +
-                '}';
-    }
 }
--- src/main/java/component/domain/model/User.java
+++ src/main/java/component/domain/model/User.java
@@ -1,12 +1,14 @@
 package component.domain.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.Objects;
+import lombok.ToString;
 
 @Getter
 @Setter
+@EqualsAndHashCode
+@ToString
 public class User {
 
     private Integer id;
@@ -18,53 +20,4 @@ public class User {
     private Integer numberOfFollowers;
     private Integer numberOfRepos;
     private Double calculations;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof User)) return false;
-
-        User user = (User) o;
-
-        if (!Objects.equals(id, user.id)) return false;
-        if (!Objects.equals(login, user.login)) return false;
-        if (!Objects.equals(name, user.name)) return false;
-        if (!Objects.equals(type, user.type)) return false;
-        if (!Objects.equals(avatarUrl, user.avatarUrl)) return false;
-        if (!Objects.equals(createdAt, user.createdAt)) return false;
-        if (!Objects.equals(numberOfFollowers, user.numberOfFollowers))
-            return false;
-        if (!Objects.equals(numberOfRepos, user.numberOfRepos))
-            return false;
-        return Objects.equals(calculations, user.calculations);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (login != null ? login.hashCode() : 0);
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (type != null ? type.hashCode() : 0);
-        result = 31 * result + (avatarUrl != null ? avatarUrl.hashCode() : 0);
-        result = 31 * result + (createdAt != null ? createdAt.hashCode() : 0);
-        result = 31 * result + (numberOfFollowers != null ? numberOfFollowers.hashCode() : 0);
-        result = 31 * result + (numberOfRepos != null ? numberOfRepos.hashCode() : 0);
-        result = 31 * result + (calculations != null ? calculations.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", login='" + login + '\'' +
-                ", name='" + name + '\'' +
-                ", type='" + type + '\'' +
-                ", avatarUrl='" + avatarUrl + '\'' +
-                ", createdAt='" + createdAt + '\'' +
-                ", numberOfFollowers=" + numberOfFollowers +
-                ", numberOfRepos=" + numberOfRepos +
-                ", calculations=" + calculations +
-                '}';
-    }
 }
